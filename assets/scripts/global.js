1147184856e6703eab37d928cf25ecae8568b8f7
Reza Firdaus <mohammad.rezafirdaus@codapayments.com>
2025-04-29 12:57:25 +0700

    update: manifest.json, color-brand styling, translation for manifest

--- assets/scripts/global.js
+++ assets/scripts/global.js
@@ -2,6 +2,7 @@ document.documentElement.setAttribute("data-theme", "light");
 document.addEventListener("DOMContentLoaded", () => {
 	const hamburgBtn = document.getElementById("hamburger-btn");
 	const mobileDrawer = document.getElementById("mobile-drawer");
+	const infobars = document.querySelectorAll(".infobar-component");
 
 	hamburgBtn.addEventListener("click", () => {
 		if (mobileDrawer.classList.contains("hidden")) {
@@ -12,4 +13,14 @@ document.addEventListener("DOMContentLoaded", () => {
 			hamburgBtn.classList.remove("open");
 		}
 	});
+
+	if (infobars) {
+		infobars.forEach((bar) => {
+			const closeButton = bar.querySelector(".infobar-dismiss");
+
+			closeButton.addEventListener("click", () => {
+				bar.classList.add("hidden");
+			});
+		});
+	}
 });
