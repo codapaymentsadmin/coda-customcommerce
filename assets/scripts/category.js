82d7a13315c96bc3cb420cc729dac9e4373cb88e
Reza Firdaus <mhmmdrezalif@gmail.com>
2026-01-06 09:54:26 +0700

    fix(1.1.1): notranslate class, searchBtn text color

--- assets/scripts/category.js
+++ assets/scripts/category.js
@@ -33,7 +33,7 @@ document.addEventListener('DOMContentLoaded', function () {
     // Style breadcrumbs
     setBreadcrumbsStyle(theme);
     // Style search button
-    setSearchBtnStyle(theme);
+    setSearchBtnStyle(theme, false);
     // Apply eFootball theming to subsection buttons
     setSubsectionBtnStyle(theme);
     // Apply game-specific theming to article buttons
